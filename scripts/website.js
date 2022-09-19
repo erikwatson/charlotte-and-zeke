4469db8eda73d3fb094301a9bda1bc70ec02a293
Erik Watson <championchap@gmail.com>
2022-09-19 18:13:39 +0100

    adding a dark overlay behind the menu when it opens

--- scripts/website.js
+++ scripts/website.js
@@ -1,11 +1,18 @@
 const sidebar = document.getElementById('sidebar')
+const overlay = document.getElementById('dark-overlay')
 
 document.addEventListener('click', evt => {
   if (sidebar.classList.contains('closed')) {
     sidebar.classList.remove('closed')
     sidebar.classList.add('open')
+
+    overlay.classList.remove('closed')
+    overlay.classList.add('open')
   } else {
     sidebar.classList.remove('open')
     sidebar.classList.add('closed')
+
+    overlay.classList.remove('open')
+    overlay.classList.add('closed')
   }
 })
